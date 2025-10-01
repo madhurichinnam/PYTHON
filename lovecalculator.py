3d706c58860e81909e1cafc40de55fac7855c6f0
madhurichinnam <madhurichinnam622@gmail.com>
2025-10-01 19:07:07 +0530

    Create lovecalculator.py

---
+++ lovecalculator.py
@@ -0,0 +1,29 @@
+# Enter your code here
+
+name1=(input("enter your name in capital:"))
+name2=(input("enter your patner name:"))
+Bothnames=name1+name2
+small_letters=Bothnames.lower()
+t=small_letters.count('t')
+r=small_letters.count('r')
+e=small_letters.count('e')
+l=small_letters.count('l')
+o=small_letters.count('o')
+v=small_letters.count('v')
+e=small_letters.count('e')
+true=t+r++u+e
+
+l=small_letters.count('l')
+o=small_letters.count('o')
+v=small_letters.count('v')
+e=small_letters.count('e')
+love=l+o+v+e
+
+lovescore=str(true)+str(love)
+if lovescore < 10 or  lovescore > 90:
+    print("You are Made for each other")
+elif lovescore<=40 or lovescore>=50:
+    print("You are very luckly to have your patner")
+else:
+    print("You are the Unique couple in the World")
+    
